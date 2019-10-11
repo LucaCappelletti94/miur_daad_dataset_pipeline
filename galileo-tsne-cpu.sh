b573fc4ff5dbd7a8cd60022bcab59f33d646c6ce
Luca Cappelletti <lcappel1@r033c01s04.galileo.cineca.it>
2019-10-11 10:43:52 +0200

    Rendered 3d mca and tsne

--- galileo-tsne-cpu.sh
+++ galileo-tsne-cpu.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=gll_usr_prod
 #SBATCH --cpus-per-task=36
 #SBATCH --mem=100GB
-#SBATCH --time 3:00:00
+#SBATCH --time 24:00:00
 #SBATCH --job-name=CNN-CPU-VISUALIZATION
 BASEDIR="/gpfs/work/ELIX4_valentin/mlp_vs_cnn/miur_daad_dataset_pipeline"
 VENVPYTHONDIR="/gpfs/work/ELIX4_valentin/mlp_vs_cnn/mlp_vs_cnn_venv"
